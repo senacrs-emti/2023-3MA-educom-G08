1bd7509f5568c930635bdc373b8ea20168724362
aueiikkkj <hugoplusvh@gmail.com>
2023-10-10 08:29:58 -0300

    update first page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.app.appsanguineo"
-    compileSdk = 33
+    compileSdk = 32
 
     defaultConfig {
         applicationId = "com.app.appsanguineo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 32
         versionCode = 1
         versionName = "1.0"
 
