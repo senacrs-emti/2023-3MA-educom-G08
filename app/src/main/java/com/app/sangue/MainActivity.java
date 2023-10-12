b5d1e79dddbe5036b122e05024c621435a55bb4c
pietrorpedro <pietrorpedro@gmail.com>
2023-10-12 12:43:11 -0300

    Main Activity

--- app/src/main/java/com/app/sangue/MainActivity.java
+++ app/src/main/java/com/app/sangue/MainActivity.java
@@ -2,13 +2,24 @@ package com.app.sangue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIniciar = findViewById(R.id.btnIniciar);
+        btnIniciar.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+            startActivity(intent);
+        });
     }
 }
