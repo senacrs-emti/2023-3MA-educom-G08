c8a4ab4e7da9c425150f1c93ad938ea5e7b74e06
pietrorpedro <pietrorpedro@gmail.com>
2023-10-12 11:33:36 -0300

    Menu & Main activity

--- app/src/main/java/com/app/appsanguineo/MainActivity.java
+++ app/src/main/java/com/app/appsanguineo/MainActivity.java
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnIniciar;
+    ImageView btnIniciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/app/appsanguineo/MenusActivity.java
+++ app/src/main/java/com/app/appsanguineo/MenusActivity.java
@@ -5,43 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 public class MenusActivity extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btn4;
+    ImageView btnMenuConteudo, btnMenuCompatibilidade, btnMenuComoDoar, btnMenuQuiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menus);
 
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);
+        btnMenuConteudo = findViewById(R.id.btnMenuConteudo);
+        btnMenuCompatibilidade = findViewById(R.id.btnMenuCompatibilidade);
+        btnMenuComoDoar = findViewById(R.id.btnMenuComoDoar);
+        btnMenuQuiz = findViewById(R.id.btnMenuQuiz);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btnMenuConteudo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MenusActivity.this, ConteudoActivity.class);
                 startActivity(intent);
             }
         });
-        btn2.setOnClickListener(new View.OnClickListener() {
+        btnMenuCompatibilidade.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MenusActivity.this, CompatibilidadeActivity.class);
                 startActivity(intent);
             }
         });
-        btn3.setOnClickListener(new View.OnClickListener() {
+        btnMenuComoDoar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MenusActivity.this, MapaActivity.class);
                 startActivity(intent);
             }
         });
-        btn4.setOnClickListener(new View.OnClickListener() {
+        btnMenuQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MenusActivity.this, QuizActivity.class);
