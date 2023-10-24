62c26315150ebef154e5157785cf4d206551a64b
pietrorpedro <pietrorpedro@gmail.com>
2023-10-23 21:41:46 -0300

    Fixed menu & exit button :p

--- app/src/main/java/com/app/sangue/MenuActivity.java
+++ app/src/main/java/com/app/sangue/MenuActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.app.sangue.Mapa.MapaActivity;
@@ -13,6 +14,7 @@ import com.app.sangue.quiz.QuizActivity;
 public class MenuActivity extends AppCompatActivity {
 
     ImageView btnMenuConteudo, btnMenuCompatibilidade, btnMenuComoDoar, btnMenuQuiz;
+    Button btnSairApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MenuActivity extends AppCompatActivity {
         btnMenuCompatibilidade = findViewById(R.id.btnMenuCompatibilidade);
         btnMenuComoDoar = findViewById(R.id.btnMenuComoDoar);
         btnMenuQuiz = findViewById(R.id.btnMenuQuiz);
+        btnSairApp = findViewById(R.id.btnSairApp);
 
         btnMenuConteudo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +53,14 @@ public class MenuActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MenuActivity.this, QuizActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnSairApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Fecha a atividade atual
+                System.exit(0); // Fecha o aplicativo
             }
         });
     }
